499f70dc340aa603e948894a7fac62d86a5f9c79
Pablo Schrut <pabloschrut@MacBook-Pro-de-Pablo.local>
2024-03-24 10:46:43 -0500

    Connection string in .env

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:edifier793@localhost/atlas'
+    load_dotenv()
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_ALCHEMY_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.init_app(app)
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -5,5 +5,5 @@ from app.routes.data_process import transactions, balance
 transactions_bp = Blueprint('transactions', __name__)
 transactions_bp.add_url_rule('/upload_transactions', 'upload_transactions', upload_transactions, methods=['POST'])
 transactions_bp.add_url_rule('/transactions', 'transactions', transactions)
-transactions_bp.add_url_rule('/test', 'test', test_endpoint)
 transactions_bp.add_url_rule('/balance', 'balance', balance)
+transactions_bp.add_url_rule('/test', 'test', test_endpoint)
